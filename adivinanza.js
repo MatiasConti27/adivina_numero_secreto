e8cb61a64a8800421777af949c06d9a364705cf8
Matias Conti <113070062+MatiasConti27@users.noreply.github.com>
2023-12-25 22:22:30 -0300

    Update adivinanza.js

--- adivinanza.js
+++ adivinanza.js
@@ -1,16 +1,18 @@
 const generarNumeroAleatorio = () => {
     return Math.floor(Math.random() * 100) + 1;
-    };
-    const verificarAdivinanza = (numeroSecreto, numeroAdivinado) => {
+};
+
+const verificarAdivinanza = (numeroSecreto, numeroAdivinado, intentos) => {
     if (numeroAdivinado == numeroSecreto) {
-    console.log('¡Felicitaciones! ¡Adivinaste el número secreto!');
-    }
-     else if (numeroAdivinado > numeroSecreto) {
-    console.log('El número secreto es menor. ¡Sigue intentando!');
+        console.log('¡Felicitaciones! ¡Adivinaste el número secreto!');
+        // Terminar el programa después de adivinar correctamente
+        return true;
+    } else if (numeroAdivinado > numeroSecreto) {
+        console.log('El número secreto es menor. ¡Sigue intentando!');
     } else {
-    console.log('El número secreto es mayor. ¡Sigue intentando!');
+        console.log('El número secreto es mayor. ¡Sigue intentando!');
     }
-    };
+}
     module.exports = {
     generarNumeroAleatorio,
     verificarAdivinanza
